c506a90d23f95ae272e15498280a6a57733ecab7
VIMALRAJ D <vimalrajnov172005@gmail.com>
2026-03-12 10:10:32 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -44,7 +44,7 @@ const projects = [
     image: "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?w=600&h=400&fit=crop",
     demoVideo: "#",
     github: "https://github.com/Vimalraj-D/Book-Recommendation",
-    live: "#",
+    live: "https://huggingface.co/spaces/Vimalraj-D/BookSage_AI",
   },
 ];
 
